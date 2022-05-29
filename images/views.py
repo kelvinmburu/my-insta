e80ceeaf13ee8efb5530c2cf878e50f68a7a3752
kelvinmburu <mburuhkelvin@gmail.com>
2022-05-30 01:58:55 +0300

    feat: added category object for db

--- images/views.py
+++ images/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 def home(request):
-    return render(request, 'main/index.html', {})
+    categories = Category.objects.all()
+    
+    context = {}
+    context['categories'] = categories
+    return render(request, 'main/index.html', context)
+
+
+def category_page(request):
+    return render(request, '', {})
