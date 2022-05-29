bc0931fd73c5df32392bae7ac8f6d869f028f4a9
kelvinmburu <mburuhkelvin@gmail.com>
2022-05-29 20:54:28 +0300

    refactor: imported custom app

--- main/settings.py
+++ main/settings.py
@@ -38,6 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Third party libraries
+    
+    # Our own custom apps
+    'images',
 ]
 
 MIDDLEWARE = [
