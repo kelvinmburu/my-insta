c1f76b0e64d34051e1c72893894df5598e8a6d20
kelvinmburu <mburuhkelvin@gmail.com>
2022-05-30 01:47:04 +0300

    fx: fixed file path typo

--- main/urls.py
+++ main/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     #space views
-    path('',include('main.urls'))
+    path('',include('images.urls'))
 ]
 
 if settings.DEBUG:
