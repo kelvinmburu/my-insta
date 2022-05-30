5ccd99884ca6fb7ea292f699fe168e3562e8e72f
kelvinmburu <mburuhkelvin@gmail.com>
2022-05-30 07:37:31 +0300

    feat: added func for category pages

--- images/views.py
+++ images/views.py
@@ -10,5 +10,12 @@ def home(request):
     return render(request, 'main/index.html', context)
 
 
-def category_page(request):
-    return render(request, '', {})
+def category_page(request, slug):
+    
+    category = Category.objects.get(slug=slug)
+    images = Image.objects.filter(category=category)
+    
+    context = {}
+    context['images'] = images
+        
+    return render(request, '', context)
