fa5954bec21a778879567bb0a95dd93a64fa98e6
kelvinmburu <mburuhkelvin@gmail.com>
2022-05-30 08:40:20 +0300

    feat: added path for viewing image sizes and details

--- images/urls.py
+++ images/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('category/<slug:slug>', views.category_page, name='image_category')
+    path('category/<slug:slug>', views.category_page, name='image_category'),
+    path('category/<slug:slug1>/<slug:slug2>', views.image_detail_page, name='image_detail'),
 ]
