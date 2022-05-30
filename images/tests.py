86edc8294c773da576cea387b8910593ccd01ead
kelvinmburu <mburuhkelvin@gmail.com>
2022-05-30 11:40:49 +0300

    test: run test for image class model

--- images/tests.py
+++ images/tests.py
@@ -1,4 +1,20 @@
 from django.test import TestCase
 from .models import Category,Image
+import datetime as dt
 
 # Create your tests here.
+class ImageTestClass(TestCase):
+    def setUp(self):
+        # Creating an image and saving it
+        self.image1 = Image(description='lorem ipsum dolor sit amet, consectetur adipiscing', altText='Food Illustration', hashtags='#food')
+        self.image1.save.image()
+        
+        # Creating a new category
+        self.new_category = Category(name='testing')
+        self.new_category.save()
+        
+        
+    def tearDown(self):
+        Category.objects.all().delete()
+        
+        
