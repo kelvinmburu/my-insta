9b17ece372698a7c31cefe66eeaba24928ca9dce
kelvinmburu <mburuhkelvin@gmail.com>
2022-05-30 23:16:19 +0300

    fix: fixed heroku deployment errors

--- main/settings.py
+++ main/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', False)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
