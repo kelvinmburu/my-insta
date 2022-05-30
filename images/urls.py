1bcf9fec901ba90c1e1877bad36da1db1a92ee3d
kelvinmburu <mburuhkelvin@gmail.com>
2022-05-30 08:21:24 +0300

    refactor: removed / typo

--- images/urls.py
+++ images/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('/category/<slug:slug>', views.category_page, name='image_category')
+    path('category/<slug:slug>', views.category_page, name='image_category')
 ]
